6a4219534dbcd3045f8c3e6b5974f4d174b7602c
Zahra Yasin <109916495+ZahraYasin2209@users.noreply.github.com>
2025-04-21 13:27:34 +0500

    Add files via upload

---
+++ Move Zeros.cpp
@@ -0,0 +1,45 @@
+// Move Zeros
+// Problem Link: https://leetcode.com/problems/move-zeroes/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int lastNonZeroIndex = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != 0)                                    // if element is not zero
+            {
+                nums[lastNonZeroIndex] = nums[i];
+                lastNonZeroIndex++;
+            }
+        }
+
+        for(int i = lastNonZeroIndex; i < nums.size(); i++)    // start placing zero from last non-zero element
+        {
+            nums[i] = 0;
+        }
+            
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {0, 1, 0, 3, 12};
+
+    sol.moveZeroes(nums);
+
+    for (int num : nums) {
+        cout << num << " ";            // 1 3 12 0 0
+    }
+    cout << endl;
+
+    return 0;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
