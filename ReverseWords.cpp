a9735fd2ccc31c07b61e7d0def3a87db8bbf8de3
Zahra Yasin <109916495+ZahraYasin2209@users.noreply.github.com>
2025-04-25 12:45:27 +0500

    Add files via upload

---
+++ ReverseWords.cpp
@@ -0,0 +1,32 @@
+// Problem Statement: Reverse each word in a string to generate new words
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<string> reverseWords(vector<string>& words) {
+    vector<string> result;
+
+    for(int i = 0; i < words.size(); i++)
+    {
+        string word = words[i];
+        reverse(word.begin(), word.end());
+        result.push_back(word);
+    }
+    return result;
+}
+
+int main() {
+    vector<string> words = {"code", "chat", "fun"};
+    vector<string> reversedWords = reverseWords(words);
+
+    for (int i = 0; i < reversedWords.size(); i++) {
+        cout << reversedWords[i] << " ";          // Output: edoc tahc nuf
+    }
+
+    return 0;
+}
+
+// Time Complexity: O(n * k)
+// Space Complexity: O(n * k)
